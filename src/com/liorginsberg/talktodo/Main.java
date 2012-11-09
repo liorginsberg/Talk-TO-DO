22a20c8c8a9f1328fd4ce0cd207ecb78f925058a
liorgins <lginsberg9a@gamil.com>
2012-11-09 12:31:16 +0200

    added main activity

---
+++ src/com/liorginsberg/talktodo/Main.java
@@ -0,0 +1,14 @@
+package com.liorginsberg.talktodo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Main extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
