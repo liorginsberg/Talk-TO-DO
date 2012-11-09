8690d098b82abe663ea78592e7601e99d0cb47dd
unknown <lior_g@topq.ramdor.co.il>
2012-11-09 13:29:32 +0200

    added addTaskSimple activity

---
+++ src/com/liorginsberg/talktodo/AddTaskSimple.java
@@ -0,0 +1,15 @@
+package com.liorginsberg.talktodo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class AddTaskSimple extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task_simple);
+    }
+
+}
