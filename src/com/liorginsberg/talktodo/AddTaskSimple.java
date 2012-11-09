05b309ac1f9a737195c04f1a013b67990af6d294
unknown <lior_g@topq.ramdor.co.il>
2012-11-09 15:04:16 +0200

    add comment

--- src/com/liorginsberg/talktodo/AddTaskSimple.java
+++ src/com/liorginsberg/talktodo/AddTaskSimple.java
@@ -5,7 +5,7 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class AddTaskSimple extends Activity {
-	//THATS WORNG
+	//THATS WORNG ah?
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
